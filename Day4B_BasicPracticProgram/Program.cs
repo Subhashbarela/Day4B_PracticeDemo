cc3aa9258c757de32df9c884ee132d14ca86fa98
Subhash Barela <barelasubhash16@gmail.com>
2023-07-06 14:52:34 +0530

    [subhash] find unique element in array

--- Day4B_BasicPracticProgram/Program.cs
+++ Day4B_BasicPracticProgram/Program.cs
@@ -11,8 +11,31 @@ namespace Day4B_BasicPracticProgram
         static void Main(string[] args)
         {
             int[] arr = { 10, 20, 15, 10, 3, 7, 4, 15 };
-            CountDublicateElm count = new CountDublicateElm();
-            count.CountMethod(arr);
+            int len=arr.Length;
+            Console.WriteLine("1: Count Dublicacy in Array \n2: Print Unique Element");
+            Console.WriteLine("Enter youe choice from Above");
+            int choice=int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    {
+                        CountDublicateElm count = new CountDublicateElm();
+                        count.CountMethod(arr);
+                        break;
+                    }
+                    case 2:
+                    {
+                        UniqueElement unique = new UniqueElement();
+                        unique.UniqueElementMethod(arr, len);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("insert valid input ..!");
+                        break;
+                    }
+            }
+            
         Console.ReadKey();
         }
     }
---
+++ Day4B_BasicPracticProgram/UniqueElement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4B_BasicPracticProgram
+{
+    public class UniqueElement
+    {
+        public void UniqueElementMethod(int[] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] == arr[j])  //if we get dublicate element 
+                    {
+                        for (int k = j; k < n - 1; k++) // traversing from dublicate element
+                        {
+                            arr[k] = arr[k + 1]; //shifting the element to remove dublicate element
+                        }
+                        n--;
+                        //j--;
+                    }
+                }
+            }
+            for (int i = 0; i <= n - 1; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }       
+    }
+}
