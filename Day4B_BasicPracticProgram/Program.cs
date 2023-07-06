5ecf9e1a28ca3e7161e6fcf5a01d635a19af7ad0
Subhash Barela <barelasubhash16@gmail.com>
2023-07-06 15:29:54 +0530

    [subhash] Sum of given digit

--- Day4B_BasicPracticProgram/Program.cs
+++ Day4B_BasicPracticProgram/Program.cs
@@ -13,7 +13,7 @@ namespace Day4B_BasicPracticProgram
             int[] arr = { 10, 20, 15, 10, 3, 7, 4, 15 };
             int len=arr.Length;
             Console.WriteLine("1: Count Dublicacy in Array \n2: Print Unique Element \n3: Find Frequency \n4: Max And Min Value" +
-                "\n5: Triangle pattern \n6: Square pattern \n7: Reverse Class");
+                "\n5: Triangle pattern \n6: Square pattern \n7: Reverse Class \n8: Sum of digit");
             Console.WriteLine("Enter youe choice from Above");
             int choice=int.Parse(Console.ReadLine());
             switch (choice)
@@ -60,6 +60,14 @@ namespace Day4B_BasicPracticProgram
                         ReverseWorldOfString reverse = new ReverseWorldOfString();
                         //reverse.ReverseOnlyWorldOfString();
                         reverse.StringReverseMethod("bridgelabs");
+                        break;
+                    }
+                case 8:
+                    {                       
+                        SumOfGivenDigit sum = new SumOfGivenDigit();
+                        Console.WriteLine("Enter the number to find sum of digit");
+                        int num = int.Parse(Console.ReadLine());
+                        sum.SumOfDigit(num);
                         break;
                     }
                 default:
---
+++ Day4B_BasicPracticProgram/SumOfGivenDigit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4B_BasicPracticProgram
+{
+    public class SumOfGivenDigit
+    {
+        public void SumOfDigit(int num)
+        {
+            int temp = num;
+            int rem, sum = 0;
+
+            while (temp > 0)
+            {
+                rem = temp % 10;
+                sum += rem;
+                temp = temp / 10;
+            }
+            Console.WriteLine("Sum of {0} number is: {1}", num, sum);
+        }
+    }
+}
